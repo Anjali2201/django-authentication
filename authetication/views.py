ebfb3711a0350a1fb861d296997603c397d4884a
Anjali2201 <anjalikushwaha031@gmail.com>
2022-05-23 19:59:47 +0530

    first commit

---
+++ authetication/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request,"authentication/index.html")
+
+def signup(request):
+    return render(request,"authentication/signup.html")
+
+def signin(request):
+    return render(request,"authentication/signin.html")
+
+def signout(request):
+    pass
